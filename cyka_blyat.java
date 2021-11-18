04dfc1a5330ef5ac25caa7df3692fc8643107b2b
Leo Mehta <94060906+Leo-Mehta@users.noreply.github.com>
2021-11-18 12:46:05 +0100

    initial commit

---
+++ cyka_blyat.java
@@ -0,0 +1,9 @@
+public class fatherIsGone { 
+  public static void main(String[] args) { 
+        
+    String father = "Your father is gone"); 
+    if (father.contains("gone")){ 
+           System.out.println("Hahahah fatherless 😂");
+    }  
+  }
+}
